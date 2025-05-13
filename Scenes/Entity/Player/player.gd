0a3968b2e4bb242a5f887d4fce837df3c20f4617
Francis Leo <drypasta34@gmail.com>
2025-05-13 08:48:52 +0800

    Update
    Added:
    - Speed Control
    - Dashing

---
+++ Scenes/Entity/Player/player.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+#Constants
+const normalSpeed = 200.0
+const sprintSpeed = 350.0
+const jumpVelocity = -400.0
+const gravity = 1000.0
+const dashSpeed = 1000.0
+const dashDuration = 0.2
+const dashCooldown = 1.0
+
+#variables
+var dashTimer = 0.0
+var dashCooldownTimer = 0.0
+var isDashing = false
+var dashDirection = 0
+
+
+#Main Physics
+func _physics_process(delta: float) -> void:
+	var direction = 0
+	var speed = normalSpeed
+	
+	#Left / Right Input
+	if Input.is_action_pressed("ui_left"):
+		direction -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction += 1
+		
+	#Sprinting
+	if Input.is_action_pressed("Sprint") and not isDashing:
+		speed = sprintSpeed
+	
+	#Dash Input
+	if Input.is_action_just_pressed("Dash") and not isDashing and dashCooldownTimer <= 0:
+		isDashing = true
+		dashTimer = dashDuration
+		dashDirection = direction if direction != 0 else sign(velocity.x)
+		dashCooldownTimer = dashCooldown
+	
+	#Handles Dash
+	if isDashing:
+		velocity.x = dashDirection * dashSpeed
+		dashTimer -= delta
+		if dashTimer <= 0:
+			isDashing = false
+	else:
+		#Apply horizontal movement
+		velocity.x = direction * speed
+	
+	#Apply Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = jumpVelocity
+		
+	#Cooldown Countdown
+	if dashCooldownTimer > 0:
+		dashCooldownTimer -= delta
+	
+	#Move the character
+	move_and_slide()
